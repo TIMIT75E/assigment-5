cdbb3d21f17ee6507bdcb20a21368d9f89e264ea
TUMIT_HASAN <tumithasan1@gmail.com>
2025-03-01 23:31:32 +0600

    added button function

--- functionalities/index.js
+++ functionalities/index.js
@@ -1,9 +1,36 @@
-const changeColor = ["green", "blue" ,"#3752FD","#63f0f4","#4cb7ba","#4ca8ba"];
+const changeColor = ["green", "blue", "#3752FD", "#63f0f4", "#4cb7ba", "#4ca8ba"];
 let index = 0;
 
-document.getElementById("theme-btn").addEventListener("click", function() {
-  document.getElementById("body").style.background = changeColor[index];
-  index = (index + 1) % changeColor.length
+document.getElementById("theme-btn").addEventListener("click", function () {
+    document.getElementById("body").style.background = changeColor[index];
+    index = (index + 1) % changeColor.length
 });
+
+
+
+function addCartItems(butttonId, cartId) {
+    const button = document.getElementById(butttonId);
+    button.addEventListener("click", function () {
+        const times = new Date().toLocaleTimeString();
+        const cart1 = document.getElementById(cartId);
+        const newElement = document.createElement("cart-cointainer");
+        newElement.textContent = `You have Complete The Task ${cart1.textContent} at : ${times}`;
+        document.getElementById("cart-cointainer").appendChild(newElement);
+        document.getElementById(butttonId).disabled = true;
+        alert("Bord Update Successfully")
+    })
+}
+
+
+addCartItems("item-1", "cart-1");
+addCartItems("item-2", "cart-2");
+addCartItems("item-3", "cart-3");
+addCartItems("item-4", "cart-4");
+addCartItems("item-5", "cart-5");
+addCartItems("item-6", "cart-6");
+
+
+
+
 
 
